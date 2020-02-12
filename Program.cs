a17a1688c12b9f558be023720a20e10017d47ed9
uhrich <warren@sosmediacorp.com>
2020-02-12 15:22:08 -0700

    Solution for C# Exercise 4.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharp_Exercise_4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = GetInput();
+            // input += GetInput();
+            input = AddSmileyToEnd( input );
+            input = AddSmileyToBeginning( input );
+            WriteName( input );
+        }
+
+        static string GetInput()
+        {
+            Console.WriteLine( "Please input some text:" );
+            return Console.ReadLine();
+        }
+
+        static string AddSmileyToEnd( string input )
+        {
+            input = input+":)";
+            return input;
+        }
+
+        static string AddSmileyToBeginning( string input )
+        {
+            input = ":("+input;
+            return input;
+        }
+
+        static void WriteName( string input ) {
+            Console.WriteLine( input );
+        }
+    }
+}
